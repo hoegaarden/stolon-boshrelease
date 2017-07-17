0500c5ea0cd889ca2b6457063692369a349fcb5f
Hannes Hörl <hannes.hoerl@snowreporter.com>
2017-07-17 18:14:41 +0100

    Fix smoke tests

--- src/smoke-tests/main.sh
+++ src/smoke-tests/main.sh
@@ -10,83 +10,76 @@ export PGPASSWORD='<%= link('users').p('pg.su_pass') %>'
 export PATH="${PATH}:/var/vcap/packages/postgres/bin"
 
 getRandom() {
-  set +e
-  cat /dev/urandom \
-    | tr -dc 'a-zA-Z0-9' \
-    | fold -w 32 \
-    | head -n 1
-  set -e
+  cat /proc/sys/kernel/random/uuid
 }
 
 runSql() {
-  psql -A -q -t
+  psql -A -t -q -c "$1"
 }
 
 simpleSelect() {
-  echo "SELECT 'connected and got result'" \
-    | runSql
+  runSql "SELECT 1"
 }
 
 mkUser() {
-  echo "CREATE ROLE \"$1\" WITH LOGIN PASSWORD '$2'" \
-    | runSql
+  runSql "CREATE ROLE \"$1\" WITH LOGIN PASSWORD '$2'"
 }
 
 delUser() {
-  echo "DROP ROLE \"$1\"" \
-    | runSql
+  runSql "DROP ROLE \"$1\""
 }
 
 mkDb() {
-  echo "CREATE DATABASE \"$1\" WITH OWNER \"$2\"" \
-    | runSql
+  runSql "CREATE DATABASE \"$1\" WITH OWNER \"$2\""
 }
 
 delDb() {
-  echo "DROP DATABASE \"$1\"" \
-    | runSql
+  PGDATABASE=postgres runSql "DROP DATABASE \"$1\""
 }
 
 createTable() {
-  echo "CREATE TABLE \"$1\" (id serial, payload text)" \
-    | runSql
+  runSql "CREATE TABLE \"$1\" (id serial, payload text)"
 }
 
-fillTable() {
-  echo "BEGIN; INSERT INTO \"$1\" (payload) VALUES ('$2'); COMMIT;" \
-    | runSql
+putData() {
+  runSql "INSERT INTO \"$1\" (payload) VALUES ('$2')"
 }
 
 getData() {
-  echo "SELECT payload FROM \"$1\"" \
-    | runSql
+  runSql "SELECT payload FROM \"$1\""
 }
 
-main() {
+msgOk() {
+  echo '[🗸]' "$@"
+}
+
+msgErr() {
+  echo '[🗴]' "$@"
+}
+
+testSuite() {
   local proxy="$1"
 
   echo "----[ proxy: ${proxy} ]----"
 
   export PGHOST="$proxy"
 
-  simpleSelect
+  local new_user="smoke_test_user_$( getRandom )"
+  local new_db="smoke_test_db_$( getRandom )"
+  local new_pass="smoke_test_pass_$( getRandom )"
+  local new_table="smoke_test_table_$( getRandom )"
+  local new_data="smoke_test_some_data_$( getRandom )_atad_emos"
 
-  local new_user="user_$( getRandom )"
-  local new_db="db_$( getRandom )"
-  local new_pass="pass_$( getRandom )"
-  local new_table="table_$( getRandom )"
-  local new_data="some_data_$( getRandom )_atad_emos"
+  simpleSelect >/dev/null
+  msgOk 'connect and query'
 
-  mkUser "$new_user" "$new_pass" || {
-    delUser "$new_user"
-    exit 1
-  }
+  mkUser "$new_user" "$new_pass"
+  trap "delUser '$new_user'" EXIT
+  msgOk 'create user'
 
-  mkDb "$new_db" "$new_user" || {
-    delDb "$new_db"
-    delUser "$new_user"
-    exit 1
-  }
+  mkDb "$new_db" "$new_user"
+  trap "delDb '$new_db' ; delUser '$new_user'" EXIT
+  msgOk 'create db'
 
   (
     export PGPASSWORD="$new_pass"
@@ -94,21 +87,33 @@ main() {
     export PGDATABASE="$new_db"
 
     createTable "$new_table"
-    fillTable   "$new_table" "$new_data"
+    msgOk 'create table'
+
+    putData "$new_table" "$new_data"
+    msgOk 'put data'
 
     local data_read="$(getData "$new_table")"
+    msgOk 'get data'
+
     [ "$data_read" = "$new_data" ] || {
-      echo "Data expected: ${new_data}"
-      echo "Data read:     ${data_read}"
+      msgErr 'data not matching'
+      echo "      expected: ${new_data}"
+      echo "          read: ${data_read}"
+
       exit 1
     }
   )
 
   delDb "$new_db"
+  trap "delUser '$new_user'" EXIT
+  msgOk 'delete database'
+
   delUser "$new_user"
+  trap '' EXIT
+  msgOk 'delete user'
 }
 
 <% link('conn').instances.each do |proxy| %>
-main '<%= proxy.address %>'
+testSuite '<%= proxy.address %>'
 <% end %>
 
